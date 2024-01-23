8d818fb9d44d0c109237703c26b5b901ca4d3d9a
Jingyi Wang <20104728@email.wit.ie>
2024-01-23 15:50:01 +0000

    Refactor - Add type declaration file; disable redundant console logs.

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -1,71 +1,58 @@
+import { Friend, Colleague, ColleagueHistory } from './myTypes'
+
 // (Optional) Change the names below to your friends.
-interface Friend {
-    name: string;
-    phone: string;
-    age: number
-}
 
 interface Contact {
     email: string;
     extension: number
 }
-interface Colleague {
-    name: string;
-    department: string;
-    contact: Contact 
-}
-
-interface ColleagueHistory {
-    current: Colleague[],
-    former: Colleague[]
-  }
 
 const friend1: Friend = {
     name: "Paul Fleming",
     phone: "087-12345",
     age: 25,
-  };
-  
-  const friend2: Friend  = {
+};
+
+const friend2: Friend = {
     name: "Jane Costello",
     phone: "086--12345",
     age: 31,
-  };
-  
-  const friends: Friend[] = [friend1, friend2];
-  console.log(friends[1]);
-  
-  //   -------------------
-  const colleague1: Colleague = {
+};
+
+export const friends: Friend[] = [friend1, friend2];
+// console.log(friends[1]);
+
+//   -------------------
+const colleague1: Colleague = {
     name: "Ralph Graham",
     department: "Engineering",
     contact: {
-      email: "rgraham@company.com",
-      extension: 121,
+        email: "rgraham@company.com",
+        extension: 121,
     },
-  };
-  
-  const colleague2: Colleague = {
+};
+
+const colleague2: Colleague = {
     name: "Patti Burke",
     department: "Finance",
     contact: {
-      email: "pburke@company.com",
-      extension: 132,
+        email: "pburke@company.com",
+        extension: 132,
     },
-  };
-  
-  const colleague3: Colleague = {
+};
+
+const colleague3: Colleague = {
     name: "Dean Sullivan",
     department: "HR",
     contact: {
-      email: "dos@company.com",
-      extension: 125,
+        email: "dos@company.com",
+        extension: 125,
     },
-  };
-  
-  export const colleagues : ColleagueHistory = {
-  current: [colleague1, colleague2, colleague3],
-  former: [],
 };
 
-  console.log(colleagues.current[0]);
+export const colleagues: ColleagueHistory = {
+    current: [colleague1, colleague2, colleague3],
+    former: [],
+};
+
+// console.log(colleagues.current[0]);
