a9f2b9d0941d3f0595dbdc436b77f568d57abdc3
Jingyi Wang <20104728@email.wit.ie>
2024-01-23 15:38:38 +0000

    Added type-safety to colleague data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -4,6 +4,17 @@ interface Friend {
     phone: string;
     age: number
 }
+
+interface Contact {
+    email: string;
+    extension: number
+}
+interface Colleague {
+    name: string;
+    department: string;
+    contact: Contact 
+}
+
 const friend1: Friend = {
     name: "Paul Fleming",
     phone: "087-12345",
@@ -20,7 +31,7 @@ const friend1: Friend = {
   console.log(friends[1]);
   
   //   -------------------
-  const colleague1 = {
+  const colleague1: Colleague = {
     name: "Ralph Graham",
     department: "Engineering",
     contact: {
@@ -29,7 +40,7 @@ const friend1: Friend = {
     },
   };
   
-  const colleague2 = {
+  const colleague2: Colleague = {
     name: "Patti Burke",
     department: "Finance",
     contact: {
@@ -38,7 +49,7 @@ const friend1: Friend = {
     },
   };
   
-  const colleague3 = {
+  const colleague3: Colleague = {
     name: "Dean Sullivan",
     department: "HR",
     contact: {
